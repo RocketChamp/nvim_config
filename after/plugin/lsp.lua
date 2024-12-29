4fda4a3da932c99be05746a8cddf9ad5ab6da423
Daniel Ilies <iliesh.daniel@gmail.com>
2024-12-29 13:36:47 +0400

    added debug symbols, setup the tabs width

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -58,3 +58,23 @@ vim.diagnostic.config({
     virtual_text = true
 })
 
+-- setup diagnostics
+vim.diagnostic.config({ virtual_text = false })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+	callback = function()
+		if vim.lsp.buf.server_ready() then
+			vim.diagnostic.open_float()
+		end
+	end,
+})
+
+-- set up LSP signs
+for type, icon in pairs({
+	Error = "",
+	Warn = "",
+	Hint = "",
+	Info = "",
+}) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
